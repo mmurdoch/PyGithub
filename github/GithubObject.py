ffd0c65c9049a7e7365c57b328c22a5c904a0801
Matthew Murdoch <matthew.murdoch.0@gmail.com>
2012-11-05 19:31:55 +0000

    Fix 'import' capitalization...

--- github/GithubObject.py
+++ github/GithubObject.py
@@ -12,7 +12,7 @@
 # You should have received a copy of the GNU Lesser General Public License along with PyGithub.  If not, see <http://www.gnu.org/licenses/>.
 
 import datetime
-Import time
+import time
 
 import GithubException
 
